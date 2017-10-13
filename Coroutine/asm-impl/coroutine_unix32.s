4aa58902206bf88045e05b96d1ec92e16c6fe006
João Baptista de Paula e Silva <jbaptistapsilva@yahoo.com.br>
2017-10-13 11:28:17 -0300

    Optimized coroutines

--- Coroutine/asm-impl/coroutine_unix32.s
+++ Coroutine/asm-impl/coroutine_unix32.s
@@ -1,136 +1,124 @@
 .intel_syntax noprefix
 
 #  
-# This file implements context functionality for UNIX i386 systems that support
+# This file implements context functionality for UNIX x64 systems that support
 # the System V ABI
 #
- 
-    .set context_entry_point, 0
-    .set context_esp, context_entry_point + 4
+
+    .set context_esp, 0
     .set context_ebp, context_esp + 4
     .set context_ebx, context_ebp + 4
-    .set context_stack_size, context_ebx + 4
-    .set context_num_saved_variables, context_stack_size + 4
+    .set context_num_saved_variables, context_ebx + 4
 
 .section .text
     .extern malloc
     .extern free
-    
-    #
-    # coroutine_t* coroutine_new(coroutine_entry_point point, size_t stack_size)
-    # - [esp+4]: point
-    # - [esp+8]: stack_size
-    # - eax: new coroutine_t*
-    #
+  
+#=======================================================================================
+#
+# coroutine_t coroutine_new(coroutine_entry_point point, size_t stack_size)
+# - [esp+4]: point
+# - [esp+8]: stack_size
+# - eax: new coroutine_t*
+#
+#=======================================================================================
+
     .global coroutine_new
 coroutine_new:
     # allocate memory for the context data plus the stack
     mov ecx, [esp+8]
-    add ecx, context_num_saved_variables
+    add ecx, context_num_saved_variables  # allocate everything in a single chunk
     push ecx
     call malloc
-    add esp, 4
+    pop ecx
     
     # If allocation fails, return early
     test eax, eax
     jz early_return
     
-    mov ecx, [esp+4]
-    mov [eax + context_entry_point], ecx             # move the entry_point into place
+    # Initialize the stack for a stub entry point
+    lea ecx, [eax + ecx-12]
     
-    mov ecx, [esp+8]
-    mov [eax + context_stack_size], ecx      # move the stack_size into its place 
-    mov dword ptr [eax + context_esp], 0     # set the rsp to 0 (for not initialized)
+    mov edx, [esp+4]
+    mov [ecx+8], edx                       # Bottom of stack: return pointer
+    mov [ecx+4], eax                       # Middle of stack: context_t parameter
+    lea edx, entry_point                   # 
+    mov [ecx], edx                         # Top of stack: entry_point stub
+    
+    mov [eax + context_esp], ecx           # Move the stack pointer to its place
     
 early_return:
     ret
+
+#=======================================================================================
+# This implements the stub entry point for proper funcion entering
+#=======================================================================================
+entry_point:
+    # assemble the function's parameters
+    # [esp] must be the coroutine_t
+    # [esp+4] must be the void* param
+    xchg [esp], eax
+    push eax
+    call [esp+8]
     
-    #
-    # void* coroutine_resume(coroutine_t coro, void* param)
-    # - [esp+4]: coro
-    # - [esp+8]: param
-    # - eax: param returned from the entry point
-    #
+    # retrieve the parameters again
+    mov ecx, [esp]
+    
+    # invalidate the own method's stack - this is safe
+    mov esp, 0
+    
+    # yield a last time
+    jmp cryield
+
+#=======================================================================================
+#
+# void* coroutine_resume(coroutine_t coro, void* param);
+# void* coroutine_yield(coroutine_t coro, void* param);
+# - [esp+4]: coro
+# - [esp+8]: param
+# - eax: param returned from the entry point
+#
+#=======================================================================================
     .global coroutine_resume
 coroutine_resume:
-    cmp dword ptr [esp+4], 0 # Careful with nullptr's
-    jz return_nullptr
-    mov ecx, [esp+4]
-
-    cmp dword ptr [ecx + context_entry_point], 0
-    jz dont_resume_ended_coroutine
-    
-    # If the coroutine is initialized, just yield to it
-    cmp dword ptr [ecx + context_esp], 0
-    jnz coroutine_yield
-
-    # move callee-preserved registers to context
-    mov [ecx + context_ebp], ebp
-    mov [ecx + context_ebx], ebx
-
-    # move the parameter to edx
-    mov edx, [esp+8]
-
-    # CALL the function as if on the new stack, so it can be used
-    # a small remark: rsi stays the same (the void* param passed)
-    mov [ecx + context_esp], esp
-    mov esp, [ecx + context_stack_size]
-    lea esp, [esp + ecx + context_num_saved_variables]
-    
-    # call the coroutine entry point
-    push edx
-    push ecx
-    call [ecx + context_entry_point]
-    pop ecx
-    add esp, 4
-    
-    # unexchange the stack
-    mov esp, [ecx + context_esp]
-    
-    # end the coroutine
-    mov qword ptr [ecx + context_entry_point], 0
-    
-    # return the same value as the last coroutine return
-    ret
-    
-return_nullptr:
-dont_resume_ended_coroutine:
-    mov eax, 0
-    ret
-    
-    #
-    # void* coroutine_yield(coroutine_t coro, void* param)
-    # - [esp+4]: coro
-    # - [esp+8]: param
-    # - [eax]: param passed to last context_yield
-    #
     .global coroutine_yield
 coroutine_yield:
     cmp dword ptr [esp+4], 0 # Careful with nullptr's
     jz return_nullptr
     
+    # If the coroutine is ended, return
     mov ecx, [esp+4]
+    cmp dword ptr [ecx + context_esp], 0
+    jz return_nullptr
+    
+    # Set the return value as the param passed
     mov eax, [esp+8]
     
+cryield:
     # Exchange callee-saved registers
     xchg [ecx + context_esp], esp
     xchg [ecx + context_ebp], ebp
     xchg [ecx + context_ebx], ebx
-
     ret
     
-    #
-    # int is_coroutine_ended(coroutine_t coro)
-    # - [esp+4]: coro
-    # - eax: 1 if coroutine is ended, 0 if it is not
-    #
+return_nullptr:
+    mov eax, 0
+    ret
+
+#=======================================================================================
+#
+# int is_coroutine_ended(coroutine_t coro)
+# - [esp+4]: coro
+# - eax: 1 if coroutine is ended, 0 if it is not
+#
+#=======================================================================================
     .global is_coroutine_ended
 is_coroutine_ended:
     cmp dword ptr [esp+4], 0 # Careful with nullptr's
     jz coroutine_is_ended
+    
     mov ecx, [esp+4]
-
-    cmp qword ptr [ecx + context_entry_point], 0
+    cmp dword ptr [ecx + context_esp], 0
     jz coroutine_is_ended
     mov eax, 0
     ret
@@ -139,22 +127,25 @@ coroutine_is_ended:
     mov eax, 1
     ret
     
-    #
-    # void coroutine_destroy(coroutine_t coro)
-    # - [esp+4]: coro
-    #
+#=======================================================================================
+#
+# void coroutine_destroy(coroutine_t coro)
+# - edi: coro
+#
+#=======================================================================================
     .global coroutine_destroy
 coroutine_destroy:
     cmp dword ptr [esp+4], 0 # Careful with nullptr's
     jz dont_delete_unended_coroutine
 
     mov ecx, [esp+4]
-    cmp qword ptr [ecx + context_entry_point], 0
+    cmp dword ptr [ecx + context_esp], 0
     jnz dont_delete_unended_coroutine
     
+    # Free the coroutine
     push ecx
     call free
     pop ecx
 dont_delete_unended_coroutine:
     ret
-
+    
--- Coroutine/asm-impl/coroutine_unix64.s
+++ Coroutine/asm-impl/coroutine_unix64.s
@@ -4,34 +4,35 @@
 # This file implements context functionality for UNIX x64 systems that support
 # the System V ABI
 #
- 
-    .set context_entry_point, 0
-    .set context_rsp, context_entry_point + 8
+
+    .set context_rsp, 0
     .set context_rbp, context_rsp + 8
     .set context_rbx, context_rbp + 8
     .set context_r12, context_rbx + 8
     .set context_r13, context_r12 + 8
     .set context_r14, context_r13 + 8
     .set context_r15, context_r14 + 8
-    .set context_stack_size, context_r15 + 8
-    .set context_num_saved_variables, context_stack_size + 8
+    .set context_num_saved_variables, context_r15 + 8
 
 .section .text
     .extern malloc
     .extern free
-    
-    #
-    # coroutine_t coroutine_new(coroutine_entry_point point, size_t stack_size)
-    # - rdi: point
-    # - rsi: stack_size
-    # - rax: new coroutine_t*
-    #
+  
+#=======================================================================================
+#
+# coroutine_t coroutine_new(coroutine_entry_point point, size_t stack_size)
+# - rdi: point
+# - rsi: stack_size
+# - rax: new coroutine_t*
+#
+#=======================================================================================
+
     .global coroutine_new
 coroutine_new:
     # allocate memory for the context data plus the stack
     push rdi
     push rsi
-    lea rdi, [rsi + context_num_saved_variables+8]  # allocate everything in a single chunk
+    lea rdi, [rsi + context_num_saved_variables]  # allocate everything in a single chunk
     call malloc
     pop rsi
     pop rdi
@@ -40,78 +41,63 @@ coroutine_new:
     test rax, rax
     jz early_return
     
-    mov [rax + context_entry_point], rdi     # move the entry_point into place
-    mov [rax + context_stack_size], rsi      # move the stack_size into its place 
-    mov qword ptr [rax + context_rsp], 0     # set the rsp to 0 (for not initialized)
+    # Initialize the stack for a stub entry point
+    lea rsi, [rax + rsi + context_num_saved_variables-16]
+    and rsi, -16
+    sub rsi, 8
+    
+    mov [rsi+16], rdi                      # Bottom of stack: return pointer
+    mov [rsi+8], rax                       # Middle of stack: context_t parameter
+    lea rdi, entry_point                   # 
+    mov [rsi], rdi                         # Top of stack: entry_point stub
+    
+    mov [rax + context_rsp], rsi           # Move the stack pointer to its place
     
 early_return:
     ret
+
+#=======================================================================================
+# This implements the stub entry point for proper funcion entering
+#=======================================================================================
+entry_point:
+    # assemble the function's parameters
+    mov rdi, [rsp]
+    mov rsi, rax
+    call [rsp+8]
     
-    #
-    # void* coroutine_resume(coroutine_t coro, void* param)#
-    # - rdi: coro
-    # - rsi: param
-    # - rax: param returned from the entry point
-    #
+    # retrieve the parameters again
+    mov rdi, [rsp]
+    
+    # invalidate the own method's stack - this is safe
+    mov rsp, 0
+    
+    # yield a last time
+    jmp cryield
+
+#=======================================================================================
+#
+# void* coroutine_resume(coroutine_t coro, void* param);
+# void* coroutine_yield(coroutine_t coro, void* param);
+# - rdi: coro
+# - rsi: param
+# - rax: param returned from the entry point
+#
+#=======================================================================================
     .global coroutine_resume
 coroutine_resume:
-    test rdi, rdi # Careful with nullptr's
-    jz return_nullptr
-
-    cmp qword ptr [rdi + context_entry_point], 0
-    jz dont_resume_ended_coroutine
-    
-    # If the coroutine is initialized, just yield to it
-    cmp qword ptr [rdi + context_rsp], 0
-    jnz coroutine_yield
-
-    # move callee-preserved registers to context
-    mov [rdi + context_rbp], rbp
-    mov [rdi + context_rbx], rbx
-    mov [rdi + context_r12], r12
-    mov [rdi + context_r13], r13
-    mov [rdi + context_r14], r14
-    mov [rdi + context_r15], r15
-
-    # CALL the function as if on the new stack, so it can be used
-    # align the stack to 16 bytes
-    # a small remark: rsi stays the same (the void* param passed)
-    mov [rdi + context_rsp], rsp
-    mov rsp, [rdi + context_stack_size]
-    lea rsp, [rsp + rdi + context_num_saved_variables]
-    and rsp, ~16
-    add rsp, 8
-    
-    # call the coroutine entry point
-    push rdi
-    call [rdi + context_entry_point]
-    pop rdi
-    
-    # unexchange the stack
-    mov rsp, [rdi + context_rsp]
-    
-    # end the coroutine
-    mov qword ptr [rdi + context_entry_point], 0
-    
-    # return the same value as the last coroutine return
-    ret
-
-return_nullptr:
-dont_resume_ended_coroutine:
-    mov rax, 0
-    ret
-    
-    #
-    # void* coroutine_yield(coroutine_t coro, void* param)
-    # - rdi: coro
-    # - rsi: param
-    # - rax: param passed to last context_yield
-    #
     .global coroutine_yield
 coroutine_yield:
     test rdi, rdi # Careful with nullptr's
     jz return_nullptr
     
+    # If the coroutine is ended, return
+    cmp qword ptr [rdi + context_rsp], 0
+    jz return_nullptr
+    
+    # Set the return value as the param passed
+    mov rax, rsi
+    
+cryield:
     # Exchange callee-saved registers
     xchg [rdi + context_rsp], rsp
     xchg [rdi + context_rbp], rbp
@@ -120,22 +106,25 @@ coroutine_yield:
     xchg [rdi + context_r13], r13
     xchg [rdi + context_r14], r14
     xchg [rdi + context_r15], r15
-    
-    # Set the return value as the param passed
-    mov rax, rsi
     ret
     
-    #
-    # int is_coroutine_ended(coroutine_t coro)
-    # - rdi: coro
-    # - rax: 1 if coroutine is ended, 0 if it is not
-    #
+return_nullptr:
+    mov rax, 0
+    ret
+
+#=======================================================================================
+#
+# int is_coroutine_ended(coroutine_t coro)
+# - rdi: coro
+# - rax: 1 if coroutine is ended, 0 if it is not
+#
+#=======================================================================================
     .global is_coroutine_ended
 is_coroutine_ended:
-    test rax, rax
+    test rdi, rdi
     jz coroutine_is_ended
 
-    cmp qword ptr [rdi + context_entry_point], 0
+    cmp qword ptr [rdi + context_rsp], 0
     jz coroutine_is_ended
     mov rax, 0
     ret
@@ -144,18 +133,20 @@ coroutine_is_ended:
     mov rax, 1
     ret
     
-    #
-    # void coroutine_destroy(coroutine_t coro)
-    # - rdi: coro
-    #
+#=======================================================================================
+#
+# void coroutine_destroy(coroutine_t coro)
+# - rdi: coro
+#
+#=======================================================================================
     .global coroutine_destroy
 coroutine_destroy:
     test rdi, rdi
     jz dont_delete_unended_coroutine
 
-    cmp qword ptr [rdi + context_entry_point], 0
+    cmp qword ptr [rdi + context_rsp], 0
     jnz dont_delete_unended_coroutine
     call free
 dont_delete_unended_coroutine:
     ret
-
+    
