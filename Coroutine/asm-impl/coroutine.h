725aff2b5aaa6260a1a4165fc74f3661b36cfad1
João Baptista <jbaptistapsilva@yahoo.com.br>
2017-10-14 17:19:59 -0300

    Modify Coroutine project to use Windows Fibers when applicable

--- Coroutine/asm-impl/coroutine.h
+++ Coroutine/asm-impl/coroutine.h
@@ -7,17 +7,11 @@ extern "C" {
 typedef volatile void* coroutine_t;
 typedef const void* (*coroutine_entry_point)(coroutine_t, const void*);
 
-#if _WIN32 && _M_IX86
-#define CDECL __cdecl
-#else
-#define CDECL
-#endif
-
-coroutine_t CDECL coroutine_new(coroutine_entry_point point, size_t stack_size);
-const void* CDECL coroutine_resume(coroutine_t coro, const void* param);
-const void* CDECL coroutine_yield(coroutine_t coro, const void* param);
-int CDECL is_coroutine_ended(coroutine_t coro);
-void CDECL coroutine_destroy(coroutine_t coro);
+coroutine_t coroutine_new(coroutine_entry_point point, size_t stack_size);
+const void* coroutine_resume(coroutine_t coro, const void* param);
+const void* coroutine_yield(coroutine_t coro, const void* param);
+int is_coroutine_ended(coroutine_t coro);
+void coroutine_destroy(coroutine_t coro);
 
 #ifdef __cplusplus
 }
--- Coroutine/main.c
+++ Coroutine/main.c
@@ -4,39 +4,39 @@
 
 const void* entry_point(coroutine_t parent, const void* value)
 {
-    int n = 0;
-    printf("coroutine receives %s from main (n = %d)\n", value, n);
-    n++;
-    value = coroutine_yield(parent, "cr1");   
-    printf("coroutine receives %s from main (n = %d)\n", value, n);
-    n++;
-    value = coroutine_yield(parent, "cr2");
-    printf("coroutine receives %s from main (n = %d)\n", value, n);
-    n++;
-    value = coroutine_yield(parent, "cr3");
-    printf("coroutine receives %s from main (n = %d)\n", value, n);
-    return "cr4";
+	int n = 0;
+	printf("coroutine receives %s from main (n = %d)\n", value, n);
+	n++;
+	value = coroutine_yield(parent, "cr1");   
+	printf("coroutine receives %s from main (n = %d)\n", value, n);
+	n++;
+	value = coroutine_yield(parent, "cr2");
+	printf("coroutine receives %s from main (n = %d)\n", value, n);
+	n++;
+	value = coroutine_yield(parent, "cr3");
+	printf("coroutine receives %s from main (n = %d)\n", value, n);
+	return "cr4";
 }
 
 int main()
 {
-    // Small text
-    coroutine_t coro = coroutine_new(entry_point, 4096);
+	// Small text
+	coroutine_t coro = coroutine_new(entry_point, 4096);
     
-    const void* value;
-    int m = 7;
-    value = coroutine_resume(coro, "mn1");
-    printf("main receives %s from coroutine (m = %d)\n", value, m);
-    m--;
-    value = coroutine_resume(coro, "mn2");
-    printf("main receives %s from coroutine (m = %d)\n", value, m);
-    m--;
-    value = coroutine_resume(coro, "mn3");
-    printf("main receives %s from coroutine (m = %d)\n", value, m);
-    m--;
-    value = coroutine_resume(coro, "mn4");
-    printf("main receives %s from coroutine (m = %d)\n", value, m);
+	const void* value;
+	int m = 7;
+	value = coroutine_resume(coro, "mn1");
+	printf("main receives %s from coroutine (m = %d)\n", value, m);
+	m--;
+	value = coroutine_resume(coro, "mn2");
+	printf("main receives %s from coroutine (m = %d)\n", value, m);
+	m--;
+	value = coroutine_resume(coro, "mn3");
+	printf("main receives %s from coroutine (m = %d)\n", value, m);
+	m--;
+	value = coroutine_resume(coro, "mn4");
+	printf("main receives %s from coroutine (m = %d)\n", value, m);
     
-    coroutine_destroy(coro);
-    return 0;
+	coroutine_destroy(coro);
+	return 0;
 }
